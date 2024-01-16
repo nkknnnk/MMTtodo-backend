0c99ac325fc3569db9051ab48ee82700cba29fe0
Nitish Kumar <nitish@infiniticube.in>
2024-01-16 11:22:16 +0530

    db url

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 let environement = process.env.ENVIRONMENT
-let db_url = process.env.MONGO_URL
+// let db_url = process.env.MONGO_URL
+let db_url = "mongodb+srv://nk6593289:nk6593289@cluster0.gopndzw.mongodb.net/MMTtodo"
 const server = '127.0.0.1:27017'  // REPLACE WITH YOUR OWN SERVER
 const database = 'MMTtodo'           // REPLACE WITH YOUR OWN DB NAME
 const connectToDB = () => {
