786a53c8603379a5282ed51a5c53f8846fd31203
Nitish Kumar <nitish@infiniticube.in>
2024-01-16 13:08:37 +0530

    cors allowed

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectToDB = require("./config/db");
-
-const app = express();
-app.use(cors({ origin: ["http://localhost:3000","https://mm-ttodo-frontend.vercel.app", "https://devnitish.com"] }));
-
-
 const todosRoutes = require("./routes/todosRoutes");
 const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+app.use(cors({ origin: ["http://localhost:3000","https://mmttodo-frontend.vercel.app", "https://devnitish.com"] }));
+
+
 
 
 // Middleware
